9588677ddda175bfb3d80c7b00551eab01e529b1
SudhansuSekharSahoo18 <sudhansusahoo359@gmail.com>
2024-07-27 23:18:47 +0530

    migration changes

---
+++ DataAccess/Migrations/20240723191306_addedBarcodeModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedBarcodeModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Barcodes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemCode = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    ItemName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Barcodes", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Barcodes",
+                columns: new[] { "Id", "ItemCode", "ItemName", "Price", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, "101", "Barcode 1", 100, 1 },
+                    { 2, "102", "Barcode 2", 200, 1 }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDateTime",
+                value: new DateTime(2024, 7, 24, 0, 43, 5, 84, DateTimeKind.Local).AddTicks(2064));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Barcodes");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDateTime",
+                value: new DateTime(2024, 7, 1, 22, 18, 11, 693, DateTimeKind.Local).AddTicks(8095));
+        }
+    }
+}
