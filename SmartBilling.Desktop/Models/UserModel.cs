722a7e916356541dcac6e76026cc31bb7a5be4ab
SudhansuSekharSahoo18 <sudhansusahoo359@gmail.com>
2025-02-16 23:12:33 +0530

    Added desktop project

---
+++ SmartBilling.Desktop/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBilling.Desktop.Models
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ SmartBilling.Desktop/ViewModels/UserViewModel.cs
@@ -0,0 +1,52 @@
+﻿using SmartBilling.Desktop.Models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace SmartBilling.Desktop.ViewModels
+{
+    public class UserViewModel : INotifyPropertyChanged
+    {
+        private UserModel _user;
+        private string _displayName;
+
+        public UserViewModel()
+        {
+            _user = new UserModel();
+            SubmitCommand = new RelayCommand(Submit);
+        }
+
+        public string Name
+        {
+            get => _user.Name;
+            set
+            {
+                _user.Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string DisplayName
+        {
+            get => _displayName;
+            set
+            {
+                _displayName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SubmitCommand { get; }
+
+        private void Submit()
+        {
+            DisplayName = $"Hello, {Name}!";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
