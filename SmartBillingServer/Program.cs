4f3b5b08305855889f3bdb4f98c4bca6aad73fcd
SudhansuSekharSahoo18 <sudhansusahoo359@gmail.com>
2025-08-06 21:40:14 +0530

    added exception logging

--- SmartBillingServer/Program.cs
+++ SmartBillingServer/Program.cs
@@ -2,66 +2,88 @@ using DataAccess.Repository;
 using Microsoft.EntityFrameworkCore;
 using SmartBillingServer.DataAccess.Data;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddCors(options =>
+internal class Program
 {
-    options.AddDefaultPolicy(
-        policy =>
+    private static void Main(string[] args)
+    {
+        try
         {
-            policy.AllowAnyOrigin()
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    policy =>
+                    {
+                        policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                    });
+            });
+
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+            builder.Services.AddScoped<IItemRepository, ItemRepository>();
+            builder.Services.AddScoped<IBillRepository, BillRepository>();
+            builder.Services.AddScoped<IBarcodeRepository, BarcodeRepository>();
+            builder.Services.AddScoped<IApplicationConfigurationRepository, ApplicationConfigurationRepository>();
+
+            // Add configuration settings
+            // builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                builder.Configuration.AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true);
+            }
+            else
+            {
+                builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            }
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors();
+
+            app.UseHttpsRedirection();
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:3000/")
+            .AllowAnyOrigin()
             .AllowAnyMethod()
-            .AllowAnyHeader();
-        });
-});
+            .AllowAnyHeader());
 
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<IItemRepository, ItemRepository>();
-builder.Services.AddScoped<IBillRepository, BillRepository>();
-builder.Services.AddScoped<IBarcodeRepository, BarcodeRepository>();
-builder.Services.AddScoped<IApplicationConfigurationRepository, ApplicationConfigurationRepository>();
+            app.UseAuthorization();
 
-// Add configuration settings
-// builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            app.MapControllers();
 
-var app = builder.Build();
+            app.Run();
+        }
+        catch (Exception ex)
+        {
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "Logs");
+            if(!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
 
-if (app.Environment.IsDevelopment())
-{
-    builder.Configuration.AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true);
+            string filePath = Path.Combine(folderPath, $"log{DateTime.Now.ToFileTime()}.txt");
+            File.AppendAllText(filePath, ex.Message);
+            throw;
+        }
+        
+    }
 }
-else
-{
-    builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-}
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseCors();
-
-app.UseHttpsRedirection();
-
-app.UseCors(builder => builder.WithOrigins("http://localhost:3000/")
-.AllowAnyOrigin()
-.AllowAnyMethod()
-.AllowAnyHeader());
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
